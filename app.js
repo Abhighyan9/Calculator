f32f48ba03566c4d0c98478ef2ff00188d583b83
Abhighyan9 <abhighyan.bommerla@gmail.com>
2021-12-04 19:40:56 +0530

    js

--- app.js
+++ app.js
@@ -14,15 +14,17 @@
     });
     
     equal.addEventListener('click', function(e){
-      if(screen.value === ''){
-        screen.value = 'Please Enter a Value';
-      } else {
-        let answer = eval(screen.value);
-        screen.value = answer;
-      }
-    })
-    clear.addEventListener('click', function(e){
-        screen.value = '';
+        if(screen.value === ''){
+          screen.value = 'Please Enter a Value';
+        } else {
+          let answer = eval(screen.value);
+          screen.value = answer;
+        }
       })
-     
-    })(); 
+      clear.addEventListener('click', function(e){
+          screen.value = '';
+        })
+       
+      })();
+    
+ 
